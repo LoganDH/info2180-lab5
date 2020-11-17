c8e9a6133abaf90f4f16f4cd622a0f00810c13a3
Logan Halsall <logandhalsall@outlook.com>
2020-11-17 02:10:50 -0500

    should be complete

--- world.js
+++ world.js
@@ -1,21 +1,36 @@
 var searchBox;
 var searchBtn;
+var searchBtnC;
 var resultSection;
 
 window.onload = function() {
     searchBox = document.getElementById("country");
     searchBtn = document.getElementById("lookup");
+    searchBtnC = document.getElementById("lookup_city");
     searchBtn.addEventListener("click", searchBtnHandler);
+    searchBtnC.addEventListener("click", searchBtnCHandler);
     resultSection = document.getElementById("result");
 }
 
 function searchBtnHandler(e){
     e.preventDefault();
-    console.log("Button clicked");
-    console.log(searchBox.value);
     fetch("world.php", {
         method: 'POST',
         body: searchBox.value.trim(),
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+   })
+   .then(response => response.text())
+   .then(data => resultSection.innerHTML = data);
+}
+
+function searchBtnCHandler(e){
+    e.preventDefault();
+    let searchValue = "cities"+searchBox.value.trim();
+    fetch("world.php", {
+        method: 'POST',
+        body: searchValue,
         headers: {
             'Content-Type': 'text/plain'
         }
